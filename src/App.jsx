68dc415101d63aa5fda81bed3ad377d19154136e
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-18 10:05:53 +0600

    Merge remote changes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div><h1 className='text-3xl'>Welcome to my website</h1>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import App from '../App';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <App/>
+        </div>
+    );
+};
+
+export default MainLayout;
