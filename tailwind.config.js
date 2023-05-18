b5f9d1f534c957f1a3cbea3b2f6bfd20e4781bc2
mustaqimbd <mustaqimkhanbd@gmail.com>
2023-05-19 00:26:34 +0600

    Banner added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('flowbite/plugin')
+]
 }
 
